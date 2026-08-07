d3e2249d2fcd480d8161378ad3c602fce7ea2bbf
karlitto__ <basementlifts@gmail.com>
2026-08-07 22:58:22 +0200

    Customize layout to include LastUpdated component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import LastUpdated from './LastUpdated.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(LastUpdated)
+    })
+  }
+}
